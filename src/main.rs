d99fe3f27ca50ecd60e08b32103d858629dd105f
uzurpastor <sheva0192@gmail.com>
2022-02-11 21:53:24 +0200

    init workspase
    Signed-off-by: uzurpastor <sheva0192@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,55 @@
-struct Philosopher {
-    name: String,
+use std::io;
+
+
+fn usize_input(prompt: String) -> usize{
+
+    let mut input_text = String::new();
+    loop{
+        println!("{prompt}");
+        io::stdin()
+            .read_line(&mut input_text)
+            .expect("failed to read from stdin");
+
+        let trimmed = input_text.trim();
+        match trimmed.parse::<usize>() {
+            Ok(i) => return i,
+            Err(..) => println!("this was not an integer, try again"),
+        };
+    }
 }
 
-impl Philosopher {
-    fn new(name: &str) -> Philosopher {
-        Philosopher {
-            name: name.to_string(),
+
+fn cesar_init(word: Vec<char>, alfabet:Vec<char>, step: usize) -> Vec<char>{
+   
+    let strong_of_alfabet = alfabet.len();
+    let mut ret: Vec<char> = ""
+        .chars()
+        .collect();
+
+    for i_chars in word.iter() {
+        for j in 0..alfabet.len(){
+            if i_chars == &alfabet[j]{
+                ret.push(alfabet[(j+step)%strong_of_alfabet]);
+            }
         }
     }
-
-    fn eat(&self) {
-        println!("{} закончила есть.", self.name);
-    }
+    ret
 }
 
-fn main() {
-    let philosophers = vec![
-        Philosopher::new("Джудит Батлер"),
-        Philosopher::new("Рая Дунаевская"),
-        Philosopher::new("Зарубина Наталья"),
-        Philosopher::new("Эмма Гольдман"),
-        Philosopher::new("Анна Шмидт"),
-    ];
 
-    for p in &philosophers {
-        p.eat();
-    }
+fn main() {
+
+    let alfabet: Vec<char> = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ"
+        .chars()
+        .collect();
+
+    let word: Vec<char> = "КРИПТОГРАФІЯ"
+        .chars()
+        .collect();
+
+    let step: usize = usize_input("input step for crypto".to_string()); 
+
+    println!(" - {:?}", word);
+    let cesar_word: Vec<char> = cesar_init(word, alfabet, step);//костыль
+    println!(" + {:?}", cesar_word);
 }
--- target/debug/cargo_workspase.d
+++
@@ -1,1 +0,0 @@
-/home/bydlo/Documents/3курс/OS/rust/cargo_workspase/target/debug/cargo_workspase: /home/bydlo/Documents/3курс/OS/rust/cargo_workspase/src/main.rs
