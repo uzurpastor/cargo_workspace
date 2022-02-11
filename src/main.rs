2d360eeb245d87e331426a09df2cd18c145e83b4
uzurpastor <sheva0192@gmail.com>
2022-02-11 22:37:25 +0200

    change args of fn usize_input: change type String to &str for comfortable use fn
    Signed-off-by: uzurpastor <sheva0192@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::io;
 
 
-fn usize_input(prompt: String) -> usize{
+fn usize_input(prompt: &str) -> usize{
 
-    let mut input_text = String::new();
+    let mut input_text = prompt.to_string();
     loop{
         println!("{prompt}");
         io::stdin()
@@ -47,7 +47,7 @@ fn cesar(){
         .chars()
         .collect();
 
-    let step: usize = usize_input("input step for crypto".to_string()); 
+    let step: usize = usize_input("input step for crypto"); 
 
     println!(" - {:?}", word);
     let cesar_word: Vec<char> = cesar_init(word, alfabet, step);//костыль
@@ -55,6 +55,47 @@ fn cesar(){
 }
 
 
+fn aphinian_cesar_chiper_logic(word: Vec<char>, alfabet: Vec<char>, k_value: usize, a_value: usize)-> Vec<char> {
+    let n_value = alfabet.len();
+    let mut ret: Vec<char> = ""
+        .chars()
+        .collect();
+
+    for i_chars in word.iter() {
+        for j in 0..alfabet.len(){
+            if i_chars == &alfabet[j]{
+                let chiper_char: char = alfabet[((k_value*j)+a_value)%n_value];
+                ret.push(chiper_char);
+            }
+        }
+    }
+    ret
+}
+
+
+fn aphinian_cesar_chiper(){
+    println!("aphinian cesar chiper:");
+    let k_value:usize = usize_input("\tinput k value: ");
+    let a_value:usize = usize_input("\tinput a value: ");
+
+    let alfabet: Vec<char> = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ"
+        .chars()
+        .collect();
+
+    let word: Vec<char> = "КРИПТОГРАФІЯ"
+        .chars()
+        .collect();
+    
+    println!("{:?}", word);
+    let cesar_word: Vec<char> = aphinian_cesar_chiper_logic(word, alfabet, k_value, a_value);//костыль
+    println!("{:?}", cesar_word);
+
+
+
+}
+
+
 fn main() {
     cesar();
+    aphinian_cesar_chiper();
 }
