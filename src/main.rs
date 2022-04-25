667d2245560a3dee5456f08198b61a24841a29ea
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-04-25 20:08:50 +0300

    make ex3 "cesar chiper with keyword"

--- src/main.rs
+++ src/main.rs
@@ -93,53 +93,97 @@ fn aphinian_cesar_chiper(){
     println!("{:?}", cesar_word);
 }
 
-////////////////////////////////////////////
 
-// fn logic_keyword_cesar_chiper(word: &Vec<char>, alfabet: &Vec<char>, k_value: usize) -> Vec<char>{
-// }
+//------------ BEGIN EX 3 ----------//
 
+fn set_key_word(default : &str) -> Vec<char>{
+	println!("Input word for chipering [default = \"{}\"]: ", default);
+    let mut input_text = String::new();
+	io::stdin()
+            .read_line(&mut input_text)
+            .expect("failed to read from stdin");
 
-fn keyword_cesar_chiper(){
-    println!("cesar with keyword:");
-    let k_value: usize = usize_input("input k value: ");
-
-    let alfabet = vec!["АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ".chars()];
-    let word = vec!["ЛІЦЕЙ".chars()];
-
-    // let chiper_word: Vec<char> = logic_keyword_cesar_chiper(&word, &alfabet, k_value);
-    let key_word = vec!["ФОРЕЛЬ".chars()];
-    
-    let key_alfabet = vec!["".chars()];
-    for i in 0..alfabet.len()+k_value{
-        if i < k_value{
-            key_alfabet.add();
-
-        }else if i >= k_value && i < k_value+key_word.len(){
-            key_alfabet[i] = key_word[i - k_value];
-
-        }else if i >= k_value+key_word.len(){
-            let mut index_alfb: usize = 0;
-            let mut ret_v = true;
-            
-            for j in key_word.iter(){
-                if assert_eq!(alfabet[index_alfb], key_word[j]){ret_v = false}
-            }
-
-            if ret_v{
-                key_alfabet[i%alfabet.len()] = alfabet[index_alfb];
-            }
-
-            index_alfb += 1;
+    let trimmed = input_text.trim();
+        match trimmed.is_empty() {
+            true => default.chars().collect(),
+            false => trimmed.chars().collect(),
         }
+}
 
+
+fn create_key_alfabet(alfabet_s : &str, step: usize) -> Vec<char>{ 
+
+    let key : Vec<char> = set_key_word("ФОРЕЛЬ");    
+    let alfabet : Vec<char>= alfabet_s.chars().collect();
+
+    let mut key_c : Vec<char> = key.clone();
+    let mut key_alfabet : Vec<char> = Vec::new();
+
+    for _i in 0..step{
+        key_alfabet.push('#');
     }
+
+    key_alfabet.append(&mut key_c);
+
+    for i in 0..alfabet.len(){
+    	let mut k : u8 = 0;
+    	for j in 0..key.len(){
+    		if alfabet[i] == key[j]{
+    			continue;
+    		}else {
+    			k += 1;
+    		}
+    	}
+
+    	if usize::from(k) == key.len(){
+    		key_alfabet.push(alfabet[i]) 
+    	};
+    }
+
+    for i in 0..step{
+    	key_alfabet[i] = key_alfabet[key_alfabet.len() - step + i];
+    }
+    key_alfabet.truncate(key_alfabet.len() - step);
+
+    key_alfabet
 
 }
 
-/////////////////////////////////////////////
+fn chiper_cesarword(key_a : Vec<char>, keystr_w : &str, alfabetstr: &str) -> Vec<char>{
+
+    let key_w : Vec<char>= keystr_w.chars().collect();
+    let alfabet : Vec<char>= alfabetstr.chars().collect();
+
+	let mut chiped_word : Vec<char> = Vec::new();
+	for i in 0..alfabet.len(){
+		for j in 0..key_w.len(){
+			if key_w[j] == alfabet[i]{
+				chiped_word.push(key_a[i]);
+			}
+		}
+	}
+	chiped_word
+
+}
+//------------ END EX 3 ----------//
+
 
 fn main(){
     cesar();
     aphinian_cesar_chiper();
+
+    //------- BEGIN EX 3 ---------//
+	println!("cesar with keyword:");
+	{
+	    let step_for_chiper: usize = usize_input("input k value: ");
+	    let alfabet : &str = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ";
+	    let key_w : &str = "ЛІЦЕЙ";
+
+	    let key_a : Vec<char> = create_key_alfabet(&alfabet, step_for_chiper);
+	    let chiped_word : Vec<char> = chiper_cesarword(key_a, &key_w, alfabet);
+
+	    println!("{:?}\n{:?}", key_w, chiped_word);
+	}
+	//------- END EX 3 ------------//
 
 }
